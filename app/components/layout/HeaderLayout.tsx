443d1c12e34e8a9489a2a27fd0b17b0adbc934c0
takkun1937 <takujin3811@gmail.com>
2024-04-16 18:52:19 +0000

    update: headerコンポーネントの作成

--- app/components/layout/HeaderLayout.tsx
+++ app/components/layout/HeaderLayout.tsx
@@ -1,5 +1,6 @@
-import { useNavigate } from "@remix-run/react";
+import { useLocation, useNavigate } from "@remix-run/react";
 import {JSX} from "react";
+import { useTranslation } from "react-i18next";
 import { ROUTE_PATH } from "~/common/constants/constants";
 
 /**
@@ -7,20 +8,26 @@ import { ROUTE_PATH } from "~/common/constants/constants";
  * @returns
  */
 export const HeaderLayout = (): JSX.Element => {
+    const {t} = useTranslation();
     const navigate = useNavigate();
+    const location = useLocation();
 
     return (
         <div className="px-8 pt-2 bg-white">
             <div>
                 <button
-                    className="px-1.5 py-1.5 rounded cursor-pointer bg-green-500 text-base font-bold text-white"
+                    className="px-1.5 py-1.5 rounded bg-green-500 text-base font-bold text-white"
                     onClick={() => navigate(ROUTE_PATH.ROOT)}>
-                        App Logo
+                         {`${t('service_name')}`}
                 </button>
             </div>
             <div>
-                <button className="h-10 px-4 pt-2 cursor-pointer hover:text-black text-sm font-semibold text-black">ワークスペース</button>
-                <button className="h-10 px-4 pt-2 cursor-pointer hover:text-black text-sm font-semibold text-gray-500">仕様書登録</button>
+                <button 
+                    className={`h-10 px-4 pt-2 text-sm font-semibold ${location.pathname === ROUTE_PATH.ROOT ? 'border-b-4 border-green-500 text-black' : 'text-gray-500 hover:text-black'}`}
+                    onClick={() => navigate(ROUTE_PATH.ROOT)}>{`${t('header.work_space')}`}</button>
+                <button 
+                    className={`h-10 px-4 pt-2 text-sm font-semibold ${location.pathname === ROUTE_PATH.REGISTER_WORK ? 'border-b-4 border-green-500 text-black' : 'text-gray-500 hover:text-black'}`}
+                    onClick={() => navigate(ROUTE_PATH.REGISTER_WORK)}>{`${t('header.register_work')}`}</button>
             </div>
         </div>
     )
