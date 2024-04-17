a5c1d820a53ffb3b15cd3bfbab3aaa4cfe91f760
takkun1937 <takujin3811@gmail.com>
2024-04-17 17:22:28 +0000

    update: フッターコンポーネントの作成

--- app/components/layout/FooterLayout.tsx
+++ app/components/layout/FooterLayout.tsx
@@ -1,13 +1,25 @@
+import { useLocation, useNavigate } from "@remix-run/react";
 import {JSX} from "react";
+import { useTranslation } from "react-i18next";
+import { ROUTE_PATH } from "~/common/constants/constants";
 
 /**
  * フッターレイアウトのコンポーネント
  * @returns
  */
 export const FooterLayout = (): JSX.Element => {
+    const {t} = useTranslation();
+    const navigate = useNavigate();
+    const location = useLocation();
+
     return (
-        <>
-            <h1>Footer</h1>
-        </>
+        <div className="flex px-8 py-12 justify-between items-center bg-gray-600">
+            <button
+                className="w-28 text-4xl font-bold text-white"
+                onClick={() => navigate(ROUTE_PATH.ROOT)}>
+                        {`${t('service_name')}`}
+            </button>
+            <label className="text-xs text-white">© 2024 Jin</label>
+        </div>
     )
 }
